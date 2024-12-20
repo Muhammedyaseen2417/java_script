2bbbe618f065ca8e26879b0f5a95774aa2c86fe6
yaseen <muhammeyaseen2417@gmail.com>
2024-12-20 09:01:36 +0530

    new

---
+++ javasript/api.js
@@ -0,0 +1,28 @@
+fetch('https://dummyjson.com/products').then(response => response.json()).then(data=>{
+    data.products.forEach(element => {
+        const row = document.querySelector(".row")
+        const col = document.createElement("div")
+        col.classList.add("col-lg-3")
+        col.innerHTML=`
+                        <div class="card" style="width: 100%">
+                <img ..." class="card-img-top" src="${element.thumbnail}" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">${element.description}</p>
+                    <a href="#" class="btn btn-primary">Get Profile</a>
+                </div>
+                </div>
+        `
+
+
+
+
+
+
+
+
+        row.appendChild(col)
+
+        
+    });
+}) 
